6eb546615690a6006f11eaa6b2130586a400c090
ddeepak95 <ddeepak95@gmail.com>
2023-05-24 08:41:51 -0700

    mobile-friendly

--- src/app/(site)/page.js
+++ src/app/(site)/page.js
@@ -1,8 +1,8 @@
 import InputForm from "./components/InputForm";
 export default function Home() {
   return (
-    <main className="p-24 bg-slate-100/50 min-h-screen">
-      <div className="px-48">
+    <main className="lg:p-12 md:p-6 p-4 xl:p-24 pb-24 bg-slate-100/50 min-h-screen">
+      <div className="xl:px-48">
         <h1 className="text-4xl text-center mb-2">
           First Day Of Class Speech Feedback
         </h1>
